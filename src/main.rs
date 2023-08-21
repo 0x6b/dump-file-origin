340b8aab3984c5bed88c188d5821aceb6fae86f7
kaoru <k@warpnine.io>
2023-08-21 18:20:01 +0900

    chore: cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::{
     error::Error,
     io::Cursor,
     iter::{empty, once},
-    path::{Path, PathBuf}
+    path::{Path, PathBuf},
 };
 
 use clap::Parser;
@@ -24,22 +24,25 @@ struct Opts {
 #[cfg(target_os = "macos")]
 fn main() -> Result<(), Box<dyn Error>> {
     let Opts { path, all } = Opts::parse();
-    let path = path.unwrap_or_else(|| home_dir().expect("failed to get home directory. Please specify a path to check specifically.").join("Downloads"));
+    let path = path.unwrap_or_else(|| {
+        home_dir()
+            .expect("failed to get home directory. Please specify a path to check specifically.")
+            .join("Downloads")
+    });
 
-    collect_files(&path)
-        .for_each(|file| {
-            let name = file.display();
-            if let Some(origin) = get_origin(&file) {
-                println!("{name}\t{}", origin);
-            } else if all {
-                println!("{name}\t(none)");
-            }
-        });
+    collect_files(&path).for_each(|file| {
+        let name = file.display();
+        if let Some(origin) = get_origin(&file) {
+            println!("{name}\t{}", origin);
+        } else if all {
+            println!("{name}\t(none)");
+        }
+    });
 
     Ok(())
 }
 
-fn collect_files(path: &Path) -> Box<dyn Iterator<Item=PathBuf>> {
+fn collect_files(path: &Path) -> Box<dyn Iterator<Item = PathBuf>> {
     if path.is_file() {
         Box::new(once(path.to_path_buf()))
     } else if path.is_dir() {
@@ -48,7 +51,7 @@ fn collect_files(path: &Path) -> Box<dyn Iterator<Item=PathBuf>> {
                 .into_iter()
                 .filter_map(Result::ok)
                 .filter(|e| e.path().is_file())
-                .map(|e| e.into_path())
+                .map(|e| e.into_path()),
         )
     } else {
         Box::new(empty())
@@ -62,7 +65,11 @@ fn get_origin(file: &dyn AsRef<Path>) -> Option<String> {
         .and_then(|attr| Value::from_reader(Cursor::new(&attr[..])).ok())
         .and_then(|val| val.into_array())
         .filter(|array| array.len() == 2)
-        .and_then(|array| array.get(1).map(|v| v.as_string().map(|s| s.trim().to_string())))
+        .and_then(|array| {
+            array
+                .get(1)
+                .map(|v| v.as_string().map(|s| s.trim().to_string()))
+        })
         .flatten()
         .filter(|origin| !origin.trim().is_empty())
 }
